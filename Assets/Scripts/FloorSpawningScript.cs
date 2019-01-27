dbdaff140c1077fd2bc8cc03e9584c2ea4b9c3a3
Lucas <tixier.lucas@gmail.com>
2019-01-27 14:48:11 +0100

    Fix 1st floor

--- Assets/Scripts/FloorSpawningScript.cs
+++ Assets/Scripts/FloorSpawningScript.cs
@@ -145,6 +145,13 @@ public class FloorSpawningScript : MonoBehaviour
     // Spawn le nouveau floor
     public IEnumerator SpawnFloor(int floor)
     {
+       
+
+        
+        var rdcShuffleList = new List<int>(new int[] { 0, 1, 2, 3 });
+        Shuffle(rdcShuffleList);
+
+
         Transform container = Containers[floor];
         List<Transform> grounds = new List<Transform>(new Transform[] { container.GetChild(0), container.GetChild(1), container.GetChild(2), container.GetChild(3) });
 
@@ -169,7 +176,9 @@ public class FloorSpawningScript : MonoBehaviour
             {
                 if (floor == 0)
                 {
-                    prefabs[i] = RDC_Prefabs[UnityEngine.Random.Range(0, RDC_Prefabs.Count - 1)];
+                    // prefabs[i] = RDC_Prefabs[UnityEngine.Random.Range(0, RDC_Prefabs.Count - 1)];
+                    
+                    prefabs[i] = RDC_Prefabs[rdcShuffleList[i]];
                 }
                 else if (floor == numberOfFloors - 1)
                 {
@@ -315,5 +324,18 @@ public class FloorSpawningScript : MonoBehaviour
         }
     }
 
+    private static System.Random rng = new System.Random();
+    public static void Shuffle(List<int> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            int value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 
 }
